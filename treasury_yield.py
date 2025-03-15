001f63c2b024ce98913bd33a9129da6937fbc083
Chris <mwangichris675@gmail.com>
2025-03-15 14:22:15 +0300

    calculate rf dynamically

--- treasury_yield.py
+++ treasury_yield.py
@@ -1,15 +1,13 @@
 import pandas as pd
-import numpy as np
-from datetime import datetime
 
-
-def get_rf(start_date:int, end_date:int):
+def get_rf(start_date:str, end_date:str):
     df_yields = pd.read_csv('US10Y_yields.csv', parse_dates=['observation_date'])
-    df_yields['year'] = df_yields['observation_date'].dt.year
-    df_yields = df_yields.loc[df_yields['year'].between(start_date, end_date, inclusive='both')]
-    df_yields_annual = df_yields.groupby(by=['year'])['DGS10'].mean()
-    average_yield = round(df_yields_annual.sum() / len(df_yields_annual), 4)*0.01
-    return average_yield
+    df_yields = df_yields[df_yields['DGS10']>=0]
+    df_yields = df_yields.loc[df_yields['observation_date'].between(start_date, end_date, inclusive='both')]
+    average_yield = (df_yields['DGS10'].sum() / len(df_yields))*0.01
+    rounded_yield = round(average_yield, 5)
+    return rounded_yield
 
-rf = get_rf(2000, 2000)
-print(rf)
+if __name__=="__main__":
+    rf = get_rf(start_date='2018-01-01', end_date='2018-12-31')
+    print(rf)
